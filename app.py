ea15408c51af770bb43007bf8bda3b936ed66d4a
Jeet_Bhardwaj <TheJeetBhardwaj@users.noreply.huggingface.co>
2026-01-23 14:08:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -314,4 +314,4 @@ def live_video():
     return render_template('index.html', mode='video')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=7860)
